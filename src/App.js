c4379ab72db8930c3f80d67e4bb3d0918472aa05
JaxTeller <sorin.craciunescu84@gmail.com>
2023-08-29 11:44:51 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import { useState } from 'react';
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  let today = new Date();
+
+  function updateDate() {
+    return new Date(
+      today.setTime(today.getTime() + count * 24 * 60 * 60 * 1000)
+    ).toDateString();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='step'>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <div>Step: {step}</div>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div className='count'>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <div>Count: {count}</div>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <br />
+      <div>
+        {count === 0
+          ? `Today is ${today}`
+          : count > 0
+          ? `${count} days from today will be: ${updateDate()}`
+          : `${Math.abs(count)} days ago was: ${updateDate()}`}
+      </div>
     </div>
   );
 }
